96fd5626dd62d68e3a4a3c6b99b1cf0b53bc8175
abhirupacharya <abhirup.acharya009@gmail.com>
2023-08-28 21:59:08 +0530

    Add if conditional for not adding logs in case of unnecessary calls.

--- src/slidingWindowLog/SlidingWindowLog.java
+++ src/slidingWindowLog/SlidingWindowLog.java
@@ -6,25 +6,29 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class SlidingWindowLog {
-  int maximumRequestsPerSecond;
-  private final Queue<Instant> log = new LinkedList<>();
+    int maximumRequestsPerSecond;
+    private final Queue<Instant> log = new LinkedList<>();
 
-  protected SlidingWindowLog(int maximumRequestsPerSecond) {
-    this.maximumRequestsPerSecond = maximumRequestsPerSecond;
-  }
-
-  void isAllowed(String requestId) {
-    Instant currentTime = Instant.now();
-    Instant previousBoundary = currentTime.minus(1, ChronoUnit.SECONDS);
-
-    synchronized (log) {
-      while (!log.isEmpty() && log.element().isBefore(previousBoundary)) {
-        log.poll();
-      }
-
-      log.add(currentTime);
-      boolean isAllowed = log.size() <= maximumRequestsPerSecond;
-      System.out.println(currentTime + "\t" + requestId + "\tIsAllowed: " + isAllowed + "\t" + log);
+    protected SlidingWindowLog(int maximumRequestsPerSecond) {
+        this.maximumRequestsPerSecond = maximumRequestsPerSecond;
     }
-  }
+
+    void isAllowed(String requestId) {
+        Instant currentTime = Instant.now();
+        Instant previousBoundary = currentTime.minus(1, ChronoUnit.SECONDS);
+
+        synchronized (log) {
+            while (!log.isEmpty() && log.element().isBefore(previousBoundary)) {
+                log.poll();
+            }
+
+            boolean isAllowed = log.size() + 1 <= maximumRequestsPerSecond;
+
+            if (isAllowed) {
+                log.add(currentTime);
+            }
+
+            System.out.println(currentTime + "\t" + requestId + "\tIsAllowed: " + isAllowed + "\t" + log);
+        }
+    }
 }
